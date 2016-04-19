64d128e2c8e005c24e8f0a8928c34e9ec3428a1b
lwenke01 <lwenke@gmail.com>
2016-04-19 12:06:33 -0700

    swtiched branches

--- app/app.js
+++ app/app.js
@@ -3,37 +3,35 @@ var angular = require('angular');
 
 var app = angular.module('app', []);
 
-app.directive('customnav', function(){
+app.directive('customNav', function(){
   return {
     restrict: 'E',
-    templateUrl: 'templates/nav-bar.html',
-    controller: function(){
-      var vm = this;
-      vm .active = 'Home'
-    },
-    controllerAs: 'navbarCtrl'
+    templateUrl: './templates/nav-bar.html'
+
   };
 });
 
-app.directive('repos', function(){
-  return {
-    restrict: 'EA',
-    templateUrl: 'templates/repos.html',
-    controller: function($http) {
-      var vm = this;
-      vm.repos = [];
-      vm.starred = [];
-      $http.get('https://api.github.com/users/lwenke01/repos')
-      .then((result)=>{
-        vm.repos = result.data;
-      }, function(error){
-        console.log(error);
-      });
-      $http.get('https://api.github.com/users/lwenke01/starred')
-      .then((result)=>{
-        vm.starred = result.data;
-      });
-    },
-    controllerAs: 'repoCtrl'
-  };
-});
+// app.directive('customRepo', function(){
+//   return {
+//     restrict: 'EA',
+//     templateUrl: './templates/repo.html',
+//     controller: function($http) {
+//       var vm = this;
+//       vm.repos = [];
+//       vm.starred = [];
+//       $http.get('https://api.github.com/users/lwenke01/repos')
+//       .then((result)=>{
+//         console.log(result.data);
+//         vm.repos = result.data;
+//
+//       }, function(error){
+//         console.log(error);
+//       });
+//       $http.get('https://api.github.com/users/lwenke01/starred')
+//       .then((result)=>{
+//         vm.starred = result.data;
+//       });
+//     },
+//     controllerAs: 'repoCtrl'
+//   };
+// });
